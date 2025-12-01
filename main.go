39e240d64ca77ac4b26b765eeb012bfe380d32ae
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2025-12-01 09:48:57 -0300

    ADD: conexão com o banco de dados

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Produto struct {
 	ID   int    `json:"id"`
 	Nome string `json:"nome"`
+}
+
+func main() {
+	// Conecta ao banco de dados PostgreSQL
+	db, err := sql.Open("postgres", "host=localhost user=postgres password=postgres dbname=produtos_db sslmode=disable")
+	if err != nil {
+		fmt.Println("Erro ao conecta o banco de dados:", err)
+	}
+	defer db.Close()
 }
